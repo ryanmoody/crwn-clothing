71cdcd4092f0ab045f697c6b0d1e771316d1e4b8
Ryan Moody <ryanmoody@protonmail.com>
2023-03-15 10:02:13 -0500

    Create navigation bar

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,6 +1,6 @@
-import Directory from './components/directory/directory.component';
+import Directory from '../../components/directory/directory.component';
 
-const App = () => {
+const Home = () => {
   const categories = [
     {
       id: 1,
@@ -29,7 +29,11 @@ const App = () => {
     },
   ];
 
-  return <Directory categories={categories} />;
+  return (
+    <>
+      <Directory categories={categories} />
+    </>
+  );
 };
 
-export default App;
+export default Home;
